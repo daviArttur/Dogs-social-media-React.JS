7c0594873c4285c246149c592abfd25e6637c809
Davi Artur <daviartur2004@gmail.com>
2022-05-17 20:39:22 -0300

    Estilo Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './Components/Home';
 import Footer from './Components/Footer';
 import Login from './Components/login/Login';
 import User from './Components/user/User';
-import Photo from './Components/photo/Photo'
+import styles from './App.module.scss'
+import Photo from './Components/photo/Photo';
 import ProtectedAccess from './Components/helper/ProtectedAccess';
 import { UserStore } from './UserContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -13,28 +14,33 @@ import UserFeed from './Components/user/UserFeed';
 import NotFound from './Components/helper/NotFound';
 
 function App() {
-
   return (
-    <>
+    <div className={styles.App}>
       <BrowserRouter>
         <UserStore>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mypage/*" element={
-              <ProtectedAccess>
-                <User />
-              </ProtectedAccess>
-            } />
-            <Route path="/user/:user" element={<UserFeed />} />
-            <Route path="/photo/:id" element={<Photo />} />
-            <Route path="/login/*" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className={styles.AppBody}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/mypage/*"
+                element={
+                  <ProtectedAccess>
+                    <User />
+                  </ProtectedAccess>
+                }
+              />
+              <Route path="/user/:user" element={<UserFeed />} />
+              <Route path="/photo/:id" element={<Photo />} />
+              <Route path="/login/*" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
           <Footer />
         </UserStore>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.scss';
+import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg'
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
+    </footer>
   )
 }
 
